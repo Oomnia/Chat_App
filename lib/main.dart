f944dfcefd7bdc0e6d3c9c7ad3b29f343c044e84
Omnia Said <omniasaied13201@gmail.com>
2025-11-25 18:53:02 +0200

    Refactor authentication logic: replace LoginCubit and RegisterCubit with AuthBloc and AuthCubit, update related screens and states

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+
 import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/screens/bloc/auth_bloc.dart';
 import 'package:chat_app/screens/chatScreen.dart';
+import 'package:chat_app/screens/cubits/auth_cubit/auth_cubit.dart';
 import 'package:chat_app/screens/cubits/chat_cubit/chat_cubit.dart';
-import 'package:chat_app/screens/cubits/login_cubit/login_cubit.dart';
-import 'package:chat_app/screens/cubits/register_cubit/register_cubit.dart';
 import 'package:chat_app/screens/loginScreen.dart';
 import 'package:chat_app/screens/registerScreen.dart';
+import 'package:chat_app/simpleblocobserver.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,6 +15,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = Simpleblocobserver();
   runApp(const ChatApp());
 }
 
@@ -23,10 +26,9 @@ class ChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => LoginCubit()),
-        BlocProvider(create: (context) => RegisterCubit()),
-                BlocProvider(create: (context) => ChatCubit()),
-
+        BlocProvider(create: (context) => AuthCubitCubit()),
+        BlocProvider(create: (context) => ChatCubit()),
+        BlocProvider(create: (context) => AuthBloc()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
